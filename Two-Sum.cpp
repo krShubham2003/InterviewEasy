9ce602746539cafe958ebe5267bbc43cbcd1d44f
Shubham Kumar <149886973+krShubham2003@users.noreply.github.com>
2025-11-22 20:18:13 +0530

    [1] [Time Beats: 100.00%] [Memory Beats: 19.92%] - LeetPush

---
+++ Two-Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+
+        unordered_map<int,int>m;
+
+        for(int i=0;i<n;i++)
+        {
+            int rem=target-nums[i];
+
+            if(m.find(rem)!=m.end())
+            {
+                return {m[rem],i};
+            }
+            m[nums[i]]=i;
+        }
+        return{-1,-1};
+
+
+    }
+};
